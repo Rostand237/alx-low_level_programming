198690943fbfd5cecaafacd338464c030a5b315b
Rostand237 <tchouomegenemanel@gmail.com>
2023-02-16 04:13:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - A program that prints a line using printf function
  * Return: Always 0
  *
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	printf("Programming is like building a multilingual puzzle,\n");
 	                return (0);
 }			
