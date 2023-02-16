5dc4200e5f821342cd85ab9e454af32fcb3d46c9
Rostand237 <tchouomegenemanel@gmail.com>
2023-02-16 04:09:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry Point
+ * Return: Always 0
  *
- * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	                return (0);
 }			
