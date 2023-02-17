f92995a097797608eb4fb86e9570b2e037b7e66c
Rostand237 <tchouomegenemanel@gmail.com>
2023-02-17 03:49:51 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,8 +11,8 @@ int main(void)
 	int num;
 	char la;
 
-	for (num = 0; num < 16; num++)
-		putchar((num % 16) + '0');
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 	for (la = 'a'; la <= 'f'; la++)
 		putchar(la);
 	putchar('\n');
