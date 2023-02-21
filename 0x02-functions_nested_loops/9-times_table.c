86253e34548acac653924848aa63b7d8eb25006f
Rostand237 <tchouomegenemanel@gmail.com>
2023-02-21 02:28:46 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * time_table - Prints the 9 times table, starting with 0
+ * times_table - Prints the 9 times table, starting with 0
  * Return: empty output
  */
 void times_table(void)
