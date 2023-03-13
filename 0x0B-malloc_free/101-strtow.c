ea0c722a30fcc89141d55d58471934b6853b09f0
Rostand237 <tchouomegenemanel@gmail.com>
2023-03-13 07:24:24 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,31 +7,32 @@
  *
  *  Return: number of words
  */
-
 int count_word(char *s)
 {
 	int flag, c, w;
 
 	flag = 0;
 	w = 0;
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		flag = 0;
-		if (flag == 0)
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
 			flag = 1;
+			w++;
 		}
-		return (w);
 	}
-
+	return (w);
+}
 /**
- * strtow - splits a string into words
+ * **strtow - splits a string into words
  * @str: string to split
  *
- *  Return: pointer to an array of strings (Success)
- *  or NULL (Error)
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
-
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
